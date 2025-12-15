a83f0bd3e8eb2a03c5d46bf1a22988c38b2a813f
anand.hemadri <anand.hemadri@icloud.com>
2025-12-15 21:50:55 +0530

    fix: spotless checks formatting fixes.

--- modernised/gallery/src/test/java/net/matrix/gallery/persistence/ArtEntityPersistenceTest.java
+++ modernised/gallery/src/test/java/net/matrix/gallery/persistence/ArtEntityPersistenceTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-
 /**
  * Verifies JPA mappings for {@link ArtEntity} aggregate.
  *
